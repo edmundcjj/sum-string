97e5e068d543b8f166124dd64e78640c6b75e967
Edmund <edmundcjj@gmail.com>
2018-03-20 10:32:03 +0800

    Completed bonus 3

--- index.js
+++ index.js
@@ -42,33 +42,42 @@ if (process.argv.length > 2) {
 
 function addNumbers(){
   var sum_total = 0;
+
   let value = process.argv[3];
   var number_arrays = value.split("");
-  console.log("Array length is : " + number_arrays.length + "\n");
-  var array_index = 0;
 
-  while (array_index < number_arrays.length) {
-    var current_value = number_arrays[array_index];
-    if (current_value === "-") {
-      console.log("Current value is negative operator");
-      var next_value = parseInt(number_arrays[array_index + 1]);
-      console.log("Current value is : -" + next_value);
-      sum_total -= next_value;
-      console.log("Current array index is: " + array_index);
-      array_index += 2;
-      console.log("Current total is :" + sum_total + "\n");
+  var regExp = /^[0-9]*$/;
+  if (regExp.test(value)) {
+    console.log("Array length is : " + number_arrays.length + "\n");
+
+    var array_index = 0;
+
+    while (array_index < number_arrays.length) {
+      var current_value = number_arrays[array_index];
+      if (current_value === "-") {
+        console.log("Current value is negative operator");
+        var next_value = parseInt(number_arrays[array_index + 1]);
+        console.log("Current value is : -" + next_value);
+        sum_total -= next_value;
+        console.log("Current array index is: " + array_index);
+        array_index += 2;
+        console.log("Current total is :" + sum_total + "\n");
+      }
+      else {
+        console.log("Current value is not negative");
+        console.log("Current value is : " + current_value);
+        var number = parseInt(current_value);
+        sum_total += number;
+        console.log("Current array index is: " + array_index);
+        array_index ++;
+        console.log("Current total is :" + sum_total + "\n");
+      }
     }
-    else {
-      console.log("Current value is not negative");
-      console.log("Current value is : " + current_value);
-      var number = parseInt(current_value);
-      sum_total += number;
-      console.log("Current array index is: " + array_index);
-      array_index ++;
-      console.log("Current total is :" + sum_total + "\n");
-    }
+    console.log("Total is: " + sum_total);
   }
-  console.log("Total is: " + sum_total);
+  else {
+    console.log("SORRY, ONLY NUMBERS ALLOWED, NO LETTERS!!~~~~");
+  }
 }
 
 function addLetters(){
