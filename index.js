98f22ef8f402d8a25aa624449220c09a21ebdba8
Edmund <edmundcjj@gmail.com>
2018-03-20 10:03:30 +0800

    Completed up till bonus 2

--- index.js
+++ index.js
@@ -1,31 +1,66 @@
 
-var sum_total = 0;
+// For exercise up till bonus 1
+// if (process.argv.length > 2) {
+//   let value = process.argv[2];
+//   var number_arrays = value.split("");
+//   console.log("Array length is : " + number_arrays.length);
+//   var array_index = 0;
+//
+//   while (array_index < number_arrays.length) {
+//     var current_value = number_arrays[array_index];
+//     if (current_value === "-") {
+//       console.log("Current value is negative operator");
+//       console.log(current_value);
+//       var next_value = parseInt(number_arrays[array_index + 1]);
+//       sum_total -= next_value;
+//       console.log("Current array index is: " + array_index);
+//       array_index += 2;
+//       console.log("Current total is :" + sum_total);
+//     }
+//     else {
+//       console.log("Current value is not negative");
+//       console.log(current_value);
+//       var number = parseInt(current_value);
+//       sum_total += number;
+//       console.log("Current array index is: " + array_index);
+//       array_index ++;
+//       console.log("Current total is :" + sum_total);
+//     }
+//   }
+//   console.log("Total is: " + sum_total);
+// }
 
+// For bonus 2
 if (process.argv.length > 2) {
-  let value = process.argv[2];
+  addNumbers();
+}
+
+function addNumbers(){
+  var sum_total = 0;
+  let value = process.argv[3];
   var number_arrays = value.split("");
-  console.log("Array length is : " + number_arrays.length);
+  console.log("Array length is : " + number_arrays.length + "\n");
   var array_index = 0;
 
   while (array_index < number_arrays.length) {
     var current_value = number_arrays[array_index];
     if (current_value === "-") {
       console.log("Current value is negative operator");
-      console.log(current_value);
       var next_value = parseInt(number_arrays[array_index + 1]);
+      console.log("Current value is : -" + next_value);
       sum_total -= next_value;
       console.log("Current array index is: " + array_index);
-      array_index ++;
-      console.log("Current total is :" + sum_total);
+      array_index += 2;
+      console.log("Current total is :" + sum_total + "\n");
     }
     else {
       console.log("Current value is not negative");
-      console.log(current_value);
+      console.log("Current value is : " + current_value);
       var number = parseInt(current_value);
       sum_total += number;
       console.log("Current array index is: " + array_index);
       array_index ++;
-      console.log("Current total is :" + sum_total);
+      console.log("Current total is :" + sum_total + "\n");
     }
   }
   console.log("Total is: " + sum_total);
